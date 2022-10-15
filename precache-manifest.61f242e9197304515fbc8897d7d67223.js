373c707cbdccac7a6f64ed50bebdc26ad29e107a
Allen Feng <clfeng@uw.edu>
2022-10-14 23:42:43 -0700

    Updates

---
+++ precache-manifest.61f242e9197304515fbc8897d7d67223.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e4ace3d5abae619414bc7d1ff8b8c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f15ea373a9929ce2bf4c",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "20dc50aacb9da0372918",
+    "url": "/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "f15ea373a9929ce2bf4c",
+    "url": "/static/js/2.83c5c4a5.chunk.js"
+  },
+  {
+    "revision": "45dd6a8eb784f4bfc84b6e54cbce993a",
+    "url": "/static/js/2.83c5c4a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20dc50aacb9da0372918",
+    "url": "/static/js/main.f5add3f3.chunk.js"
+  },
+  {
+    "revision": "bfed812ea91b71ebf252",
+    "url": "/static/js/runtime-main.8de15931.js"
+  }
+]);
